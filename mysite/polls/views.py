a5ce29d1dc3cc5eddb26eb2ff32847bb5fc217d8
cakkalyoncu <48496700+cakkalyoncu@users.noreply.github.com>
2019-11-20 17:47:07 +0300

    Improved NLP module, action extraction

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,6 +21,7 @@ def index(request):
     obj_hist.clear()
     return render(request, 'polls/index.html')
 
+
 def create_new_canvas(request):
     return HttpResponse("Started recording")
 
@@ -32,7 +33,8 @@ def recordAndDraw(request):
     obj = nlp(sentence)
     name = obj.name
     if name in museum_items:
-        return render(request, 'polls/demo.html', {'origin': cumle, 'sentence': sentence, 'obje': "/museum_imgs/"+name+".jpg"})
+        return render(request, 'polls/demo.html',
+                      {'origin': cumle, 'sentence': sentence, 'obje': "/museum_imgs/" + name + ".jpg"})
     fileName = "/Users/apple/Downloads/filtered/" + name + ".ndjson"
     f = open(fileName, "r")
     qdImages = f.read()
@@ -52,13 +54,6 @@ def draw_objects(request):
     list = objects.strip("][").split(', ')
     return render(request, 'polls/demo.html', {'object': list[0]})
 
-def nlp(sentence):
-    nlp = spacy.load('en_core_web_sm')
-    doc = nlp(sentence)
-    main_obj_token = main_object(doc)
-    obj = match_features(extract_features(doc, main_obj_token), main_obj_token)
-    return obj
-
 
 def record():
     r = sr.Recognizer()
@@ -72,7 +67,7 @@ def record():
         sentence = r.recognize_google(audio, language="tr-TR")
         translate_client = translate.Client()
         result = translate_client.translate(
-            sentence, target_language = "en")
+            sentence, target_language="en")
         translated = result['translatedText']
     except sr.UnknownValueError:
         text = "Google Speech Recognition could not understand audio"
@@ -82,18 +77,30 @@ def record():
     return context
 
 
+def nlp(sentence):
+    doc = nlp(sentence)
+    main_lst_json = []
+    main_lst = extract_main_object(doc)
+    for main_object in main_lst:
+        main_lst_json.append(match_features(extract_features(doc, main_object), main_object).to_json())
+    return main_lst_json
+    return obj
+
+
 class Object:
-    def __init__(self, name, color="black", size=1, number=1, location="random"):
+
+    def __init__(self, name, color="black", size=1, number=1, location="random", action = None):
         self.name = name
         self.color = color
         self.size = size
         self.number = number
         self.location = location
+        self.action = action
         self.strokeArray = []
 
     def print(self):
         print("Name: ", self.name, "\tColor:", self.color, " Size:", self.size, " Number:", self.number, " Location:",
-              self.location)
+              self.location, self.action)
 
     def to_json(self):
         return json.dumps(self.__dict__)
@@ -114,49 +121,66 @@ def isNumber(feature):
     except:
         return False
 
+action_map = [["fly"],["run"],["walk"]]
+def isAction(feature):
+    if(feature.lemma_ in action_map[0]):
+      return "fly"
+    elif(feature.lemma_ in action_map[1]):
+      return "run"
+    elif(feature.lemma_ in action_map[2]):
+      return "walk"
 
-def main_object(doc):
-    obj = doc[0]
-    for token in doc:
-        if(token.tag_ == "NN" and obj.tag_ !="NN"):
-            obj = token
-        if(token.dep_ == "attr"):
-            return token
-    return obj
+
+def extract_main_object(doc):
+    main_lst = [chunk.root for chunk in doc.noun_chunks if chunk.root.dep_ == "ROOT" or chunk.root.dep_ == "nsubj"]
+    conj_lst = list([main.conjuncts for main in main_lst][0])
+    return main_lst + conj_lst
+
+def get_chunks(doc, main_obj_token):
+  for chunk in doc.noun_chunks:
+    if(chunk.root == main_obj_token):
+      return [token for token in chunk if token != main_obj_token]
 
 # Extract the relevant information of the main object
 # Implement better ->
 def extract_features(doc, main_obj_token):
-  # Object of interest, dependency tag = "attr"
-  # obj = [token for token in doc if (token.dep_ == "attr")]
-  # print(type(obj))
-
-    stack = []
     feature_lst = []
-    stack=([child for child in main_obj_token.children])
-
+    stack = [ancestor for ancestor in main_obj_token.ancestors]
     while stack:
-        token = stack.pop()
-        feature_lst.append(token.lemma_)
-        if(token.pos_ != "NOUN"):
-            stack+=[child for child in token.children]
+        print(stack)
+        current = stack.pop()
+        if (current.pos_ != "NOUN"):
+            feature_lst.append(current)
+            stack += [ancestor for ancestor in current.children]
+    feature_lst += (get_chunks(doc, main_obj_token))
     return feature_lst
 
 
 # Creates an "Object" based on the given information
-def match_features(feature_lst,main_obj_token):
-    ob = Object(name = main_obj_token.text)
+def match_features(feature_lst, main_obj_token):
+    ob = Object(name=main_obj_token.lemma_)
     for feature in feature_lst:
-        if(isColor(feature)):
-            ob.color = feature
-        elif(isNumber(feature)):
-            number = w2n.word_to_num(feature)
+        feature_txt = feature.lemma_
+        size = isSize(feature_txt)
+        if (size):
+            ob.size = size
+
+        elif (isColor(feature_txt)):
+            ob.color = feature_txt
+
+        elif (isNumber(feature_txt)):
+            number = w2n.word_to_num(feature_txt)
             ob.number = number
+
+        elif (main_obj_token.tag_ == "NNS"):
+            ob.number = random.randint(2, 5)
+
+        elif (isPreposition(feature)):
+            location = [child.lemma_ for child in feature.children if (child.tag_ == "NN")][0]
+            ob.location = {"Preposition": feature.lemma_,
+                            "Location": location}
+
+        elif (isAction(feature)):
+            ob.action = feature.text
+
     return ob
-
-
-def sentence_processing(sentence):
-    doc = nlp(sentence)
-    main_obj_token = main_object(doc)
-    obj = match_features(extract_features(doc, main_obj_token), main_obj_token)
-    return obj.to_json()
